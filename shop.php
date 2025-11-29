c18a04fbcd6aa2f5fdb6a619ea130f84cfc2c8d5
Wajiha-Batool5 <wajihabatool191@gmail.com>
2025-11-29 18:43:13 +0500

    shop page added

--- shop.php
+++ shop.php
@@ -1,1 +1,32 @@
 # shop page
+<?php include "views/include/header.php"; ?>
+
+<div class="shop-page">
+    <h2>All Products</h2>
+    <div class="product-grid">
+
+        <div class="product-card">
+            <img src="assets/images/products/p4.jpg">
+            <p class="name">Stylish Kurti</p>
+            <p class="price">Rs 2500</p>
+            <a href="product_detail.php?id=4" class="btn-sm">View</a>
+        </div>
+
+        <div class="product-card">
+            <img src="assets/images/products/p5.jpg">
+            <p class="name">Black Abaya</p>
+            <p class="price">Rs 4200</p>
+            <a href="product_detail.php?id=5" class="btn-sm">View</a>
+        </div>
+
+        <div class="product-card">
+            <img src="assets/images/products/p6.jpg">
+            <p class="name">Casual Top</p>
+            <p class="price">Rs 1600</p>
+            <a href="product_detail.php?id=6" class="btn-sm">View</a>
+        </div>
+
+    </div>
+</div>
+
+<?php include "views/include/footer.php"; ?>
