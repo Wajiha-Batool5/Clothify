61aceecc3329e3098917a6ad897db944b51c9f63
Wajiha-Batool5 <wajihabatool191@gmail.com>
2025-11-29 20:16:19 +0500

    Shop page is designed

--- shop.php
+++ shop.php
@@ -1,29 +1,78 @@
-# shop page
 <?php include "views/include/header.php"; ?>
+<link rel="stylesheet" href="assets/css/style.css">
 
 <div class="shop-page">
     <h2>All Products</h2>
     <div class="product-grid">
 
         <div class="product-card">
-            <img src="assets/images/products/p4.jpg">
-            <p class="name">Stylish Kurti</p>
-            <p class="price">Rs 2500</p>
+            <img src="assets\images\products\co-ords\Black Scalloped Embroidered Co-Ord Set (2-Piece).jpg">
+            <p class="name">Black Scalloped Embroidered Co-Ord Set (2-Piece)</p>
+            <p class="price">Rs 5000</p>
+            <a href="product_detail.php?id=1" class="btn-sm">View</a>
+        </div>
+
+        <div class="product-card">
+            <img src="assets\images\products\party wear\Celeste Ethereal Powder Blue Embroidered Suit.jpg">
+            <p class="name">Celeste Ethereal Powder Blue Embroidered Suit</p>
+            <p class="price">Rs 42,000</p>
+            <a href="product_detail.php?id=2" class="btn-sm">View</a>
+        </div>
+
+        <div class="product-card">
+            <img src="assets\images\products\wedding\Ethereal Ivory & Gold Bridal Pishwas Lehenga.jpg">
+            <p class="name">Ethereal Ivory & Gold Bridal Pishwas Lehenga</p>
+            <p class="price">Rs 670,000</p>
+            <a href="product_detail.php?id=3" class="btn-sm">View</a>
+        </div>
+
+        <div class="product-card">
+            <img src="assets\images\products\co-ords\Red & White Botanical Printed Co-Ord Set (2-Piece).jpg">
+            <p class="name">Red & White Botanical Printed Co-Ord Set (2-Piece)</p>
+            <p class="price">Rs 4000</p>
             <a href="product_detail.php?id=4" class="btn-sm">View</a>
         </div>
 
         <div class="product-card">
-            <img src="assets/images/products/p5.jpg">
-            <p class="name">Black Abaya</p>
-            <p class="price">Rs 4200</p>
+            <img src="assets\images\products\party wear\Ivory White Tonal Embroidered Suit (New Outfit).jpg">
+            <p class="name">Ivory White Tonal Embroidered Suit (New Outfit)</p>
+            <p class="price">Rs 19,000</p>
             <a href="product_detail.php?id=5" class="btn-sm">View</a>
         </div>
 
         <div class="product-card">
-            <img src="assets/images/products/p6.jpg">
-            <p class="name">Casual Top</p>
-            <p class="price">Rs 1600</p>
+            <img src="assets\images\products\wedding\Luxurious Dark TealGreen Velvet Jacket & Sharara Ensemble.jpg">
+            <p class="name">Luxurious Dark TealGreen Velvet Jacket & Sharara Ensemble</p>
+            <p class="price">Rs 150,000</p>
             <a href="product_detail.php?id=6" class="btn-sm">View</a>
+        </div>
+
+        <div class="product-card">
+            <img src="assets/images/products/co-ords/Emerald Breeze Printed Co-Ord Set.jpg">
+            <p class="name">Emerald Breeze Printed Co-Ord Set</p>
+            <p class="price">Rs 8000</p>
+            <a href="product_detail.php?id=7" class="btn-sm">View</a>
+        </div>
+
+        <div class="product-card">
+            <img src="assets/images/products/party wear/Desert Rose - Camel Embroidered Suit with Deep Plum Dupatta.jpg">
+            <p class="name">Desert Rose - Camel Embroidered Suit with Deep Plum Dupatta</p>
+            <p class="price">Rs 70,000</p>
+            <a href="product_detail.php?id=8" class="btn-sm">View</a>
+        </div>
+
+        <div class="product-card">
+            <img src="assets/images/products/wedding/Pastel Grey Organza Kameez with Dusty Pink Gharara.jpg">
+            <p class="name">Pastel Grey Organza Kameez with Dusty Pink Gharara</p>
+            <p class="price">Rs 190,000</p>
+            <a href="product_detail.php?id=9" class="btn-sm">View</a>
+        </div>
+
+        <div class="product-card">
+            <img src="assets/images/products/wedding/Regal Rani Pink & Gold Layered Bridal Pishwas.jpg">
+            <p class="name">Regal Rani Pink & Gold Layered Bridal Pishwas</p>
+            <p class="price">Rs 550,000</p>
+            <a href="product_detail.php?id=10" class="btn-sm">View</a>
         </div>
 
     </div>
