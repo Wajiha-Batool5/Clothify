91985483e55221016a0301a07b9298e8488c7761
Wajiha-Batool5 <wajihabatool191@gmail.com>
2025-11-29 20:14:33 +0500

    Update product detail page

---
+++ views/products/product_detail.php
@@ -0,0 +1,43 @@
+<?php include "../../views/include/header.php"; ?>
+
+<link rel="stylesheet" href="../../assets/css/style.css">
+
+<div class="product-detail">
+    <img src="../../assets\images\products\co-ords\Black Scalloped Embroidered Co-Ord Set (2-Piece).jpg" class="detail-img">
+
+    <div class="detail-info">
+        <h2>Black Scalloped Embroidered Co-Ord Set (2-Piece)</h2>
+        <p class="price">Rs 5000</p>
+        <p class="description">
+            Elevate your everyday style with Clothify’s <strong>Black Scalloped Embroidered Co-Ord Set</strong>, designed for girls who love minimal elegance.
+            This two-piece outfit features a loose-fit kurta with delicate white scalloped embroidery on the sleeves and hem, paired with matching wide-leg trousers that offer comfort and sophistication.
+        </p>
+
+        <p class="description">
+            Crafted from soft, breathable fabric, this outfit is perfect for casual outings, university wear, brunch, or summer gatherings.
+            The clean silhouette and classic black shade make it a wardrobe essential you can style effortlessly.
+        </p>
+        <section class="features" aria-labelledby="features-heading">
+            <h3 id="features-heading">Features</h3>
+                <ul>
+                    <li>2-Piece Set: Kurta + Wide-Leg Trousers</li>
+                    <li>Premium soft &amp; breathable fabric</li>
+                    <li>White scalloped embroidery detailing</li>
+                    <li>Loose, comfortable fit</li>
+                    <li>Stylish, minimal, and elegant look</li>
+                    <li>Perfect for everyday wear</li>
+                </ul>
+        </section>
+
+        <label>Size:</label>
+        <select>
+            <option>Small</option>
+            <option>Medium</option>
+            <option>Large</option>
+        </select>
+
+        <a href="cart.php" class="btn">Add to Cart</a>
+    </div>
+</div>
+
+<?php include "views/include/footer.php"; ?>
