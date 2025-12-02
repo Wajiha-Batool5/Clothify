3257525d967c590053f98d86574942f7d759a3d4
Wajiha-Batool5 <wajihabatool191@gmail.com>
2025-12-02 19:25:52 +0500

    Update header to include stylesheet link

--- views/include/header.php
+++ views/include/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clothify - Fashion Store</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/WebPHPProject/Clothify/assets/css/style.css">
 </head>
 <body>
